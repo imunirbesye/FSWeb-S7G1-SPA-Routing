aa22ae9035abf184ad4b808091f4eb464798a2ef
Münir BESYE <imunirbesye@gmail.com>
2023-08-20 22:38:02 +0300

    yapildi

--- client/src/Filmler/KaydedilenlerListesi.js
+++ client/src/Filmler/KaydedilenlerListesi.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 
 export default function KaydedilenlerListesi(props) {
+
+  const history = useHistory();
+
+  const MainPage = () => {
+    history.push("/");
+  } 
+
   return (
     <div className="saved-list">
       <h3>Kaydedilen Filmler:</h3>
-      {props.list.map(movie => (
-        <span className="saved-movie">{movie.title}</span>
-      ))}
-      <div className="home-button">Anasayfa</div>
+      {
+        Array.isArray(props.list) ?
+          props.list.map(movie => (
+            <span className="saved-movie">{movie}</span>
+          ))
+          :
+          (<span className="saved-movie">{props.list[0]}</span>)
+        }
+      <div className="home-button" onClick={() => MainPage()}>Anasayfa</div>
     </div>
   );
 }
