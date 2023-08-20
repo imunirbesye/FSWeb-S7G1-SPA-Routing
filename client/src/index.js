069c0fbd65b44b56c827aeb1d5abb5cb9be06347
Münir BESYE <imunirbesye@gmail.com>
2023-08-20 22:37:03 +0300

    yapildi

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 import './index.css';
 import App from './App';
@@ -9,5 +10,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Router>
+        <App />
+    </Router>
 );
